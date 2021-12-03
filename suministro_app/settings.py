ec4cd5f6d22f082503b0e1b446a7c23fbfc5fe9c
Noel Moreira <informatica@passus.com.ni>
2021-12-03 17:40:41 -0600

    init5

--- suministro_app/settings.py
+++ suministro_app/settings.py
@@ -26,7 +26,7 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 SECRET_KEY = 'django-insecure-r0hjif!+z1$_t$$bpk%m+n%c0t8nx#mk9#_$klxcv%_&&@0l)1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '.herokuapp.com', 'suministro.herokuapp.com']
 
