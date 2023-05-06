b08c2f0a9f6c88076f12f0750c4baef517a18379
Moreira <noelmoreira7@gmail.com>
2023-05-06 11:51:04 -0600

    prueba

--- suministro_app/settings.py
+++ suministro_app/settings.py
@@ -26,7 +26,7 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 SECRET_KEY = 'django-insecure-r0hjif!+z1$_t$$bpk%m+n%c0t8nx#mk9#_$klxcv%_&&@0l)1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'lionfish-app-9hfmt.ondigitalocean.app']
 
